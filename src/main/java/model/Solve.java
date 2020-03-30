4c6966427c2a630ea79da64c782a72ca7fab124d
daniel <danielbobrzyk@gmail.com>
2020-03-30 23:54:52 +0200

    Added implementation of getters and setters of Solve's fields in order to work with DataBase.

--- src/main/java/model/Solve.java
+++ src/main/java/model/Solve.java
@@ -1,11 +1,21 @@
 package model;
 
 import java.sql.Time;
+import java.util.Date;
 
 public interface Solve {
-    Time extractTime(SolveImplementation solution);
-    State extractState(SolveImplementation solution);
-    String AddComment();
+    Time getTime();
+    State getState();
+    Date getDate();
+    int getID();
+    CubeType getType();
+
+    void setTime(Time time);
+    void setState(State state);
+    void setDate(Date date);
+    void setID(int id);
+    void setType(CubeType type);
+
     void rejectSolution(); // changes stato to REJ
     void Penalty(); // modifies time of solution
 }
--- src/main/java/model/SolveImplementation.java
+++ src/main/java/model/SolveImplementation.java
@@ -1,45 +1,86 @@
 package model;
 
 import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SolveImplementation implements Solve {
-   // SimpleDateFormat ft;
-  //  Date dateOfSolution; date to be added
+
+  //  SimpleDateFormat ft;
+    Date dateOfSolution;
     Time timeOfSolution;
     State whatState;
- //   String comment;
-
+    CubeType whatCube;
+    int ID;
     public SolveImplementation(){
+        dateOfSolution=null;
         timeOfSolution=null;
         whatState=State.NONE;
+        whatCube=CubeType.THREEBYTHREE;
     }
-    public SolveImplementation(long time, State s){
+    public SolveImplementation(Date date,long time, State state,CubeType type){
         timeOfSolution=new Time(time);
-        whatState=s;
+        whatState=state;
+        dateOfSolution=new Date();
+        dateOfSolution=date;
+        whatCube=type;
     }
 
     @Override
-    public Time extractTime(SolveImplementation solution) throws NullPointerException{
-        if(solution==null) throw new NullPointerException();
+    public Time getTime() {
         return timeOfSolution;
     }
 
     @Override
-    public State extractState(SolveImplementation solution) throws NullPointerException{
-        if(solution==null) throw new NullPointerException();
+    public State getState() {
         return whatState;
     }
 
     @Override
-    public String AddComment() {
-        return null;
+    public Date getDate() {
+        return dateOfSolution;
     }
+
+    @Override
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public CubeType getType() {
+        return whatCube;
+    }
+
+    @Override
+    public void setTime(Time time) {
+        this.timeOfSolution=time;
+    }
+
+    @Override
+    public void setState(State state) {
+        whatState=state;
+    }
+
+    @Override
+    public void setDate(Date date) {
+        this.dateOfSolution=date;
+    }
+
+    @Override
+    public void setID(int id) {
+        this.ID=id;
+    }
+
+    @Override
+    public void setType(CubeType type) {
+        this.whatCube=type;
+    }
+
 
     @Override
     public void rejectSolution() {
         whatState=State.REJ;
     }
-
     @Override
     public void Penalty() {
     }
