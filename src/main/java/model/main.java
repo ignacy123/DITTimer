6f439fac0aca0e5d60e195acded0512a03d09a90
daniel <danielbobrzyk@gmail.com>
2020-03-30 16:57:38 +0200

    Fixing Typo and change name pattern

--- src/main/java/model/main.java
+++ src/main/java/model/main.java
@@ -3,7 +3,7 @@ package model;
 public class main {
     public static void main(String[] args) {
         ScrambleGenerator scr = new ScrambleGeneratorImplementation(CubeType.TWOBYTWO);
-        System.out.println(scr.generate());
+        System.out.println("scr.generate()");
     }
 
 }
