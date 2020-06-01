e1b8d157159ad697fbce7060c6afdc307194caaa
tmarzec <tomasz4.marzec@student.uj.edu.pl>
2020-06-01 18:48:33 +0200

    minor fix v2

--- src/main/java/view/drawing/StatsWindow.java
+++ src/main/java/view/drawing/StatsWindow.java
@@ -1,6 +1,9 @@
 package view.drawing;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -11,19 +14,25 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
 import javafx.util.StringConverter;
+import model.SS.StatisticServer;
+import model.SS.StatisticServerImplementation;
 import model.enums.AVG;
 import model.enums.CubeType;
 import model.enums.State;
 import model.logic.Solve;
+import model.logic.SolveImplementation;
 import model.wrappers.AVGwrapper;
 import model.wrappers.ObservableWrapper;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.ResourceBundle;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalTime;
+import java.util.*;
 
 public class StatsWindow implements Initializable {
 
@@ -66,31 +75,31 @@ public class StatsWindow implements Initializable {
         //size - how much-1 (max with 0) up to size -1
         for (int j = Math.max(morszczuk.getListOfSolves(type).size() - count, 0), i = 0; j < morszczuk.getListOfSolves(type).size(); j++, i++) {
             Solve a = morszczuk.getListOfSolves(type).get(j);
-            int in = (int) a.getTime().getTime();
             if(a.getState().equals(State.DNF)) {
-                in=0;
+                i--;
+                continue;
             }
-            data.getData().add(new XYChart.Data<>(Integer.toString(i + 1), in));
+            data.getData().add(new XYChart.Data<>(Integer.toString(i + 1), (int) a.getTime().getTime()));
         }
         data.setName("all");
         XYChart.Series<String, Integer> data2 = new XYChart.Series<>();
         for (int j = Math.max(morszczuk.getListAvg(type, AVG.Ao5).size() - count, 0), i = 0; j < morszczuk.getListAvg(type, AVG.Ao5).size(); j++, i++) {
             AVGwrapper avg = morszczuk.getListAvg(type, AVG.Ao5).get(j);
-            int in = (int) avg.getAVG().getTime();
             if(avg.isDNF()) {
-                in=0;
+                i--;
+                continue;
             }
-            data2.getData().add(new XYChart.Data<>(Integer.toString(i + 1), in));
+            data2.getData().add(new XYChart.Data<>(Integer.toString(i + 1), (int) avg.getAVG().getTime()));
         }
         data2.setName("Ao5");
         XYChart.Series<String, Integer> data3 = new XYChart.Series<>();
         for (int j = Math.max(morszczuk.getListAvg(type, AVG.Ao12).size() - count, 0), i = 0; j < morszczuk.getListAvg(type, AVG.Ao12).size(); j++, i++) {
             AVGwrapper avg = morszczuk.getListAvg(type, AVG.Ao12).get(j);
-            int in = (int) avg.getAVG().getTime();
             if(avg.isDNF()) {
-                in=0;
+                i--;
+                continue;
             }
-            data3.getData().add(new XYChart.Data<>(Integer.toString(i + 1), in));
+            data3.getData().add(new XYChart.Data<>(Integer.toString(i + 1), (int) avg.getAVG().getTime()));
         }
         data3.setName("Ao12");
         chart.getData().addAll(data, data2, data3);
@@ -103,9 +112,12 @@ public class StatsWindow implements Initializable {
         types.add(CubeType.THREEBYTHREE);
         types.add(CubeType.FOURBYFOUR);
         types.add(CubeType.TWOBYTWO);
-        morszczuk.getCubeCurrType().addListener((ListChangeListener<CubeType>) change -> {
-            type = morszczuk.getCubeCurrType().get(0);
-            load();
+        morszczuk.getCubeCurrType().addListener(new ListChangeListener<CubeType>() {
+            @Override
+            public void onChanged(Change<? extends CubeType> change) {
+                type = morszczuk.getCubeCurrType().get(0);
+                load();
+            }
         });
         for (CubeType type : types) {
             morszczuk.getListOfSolves(type).addListener((ListChangeListener<Solve>) change -> load());
@@ -114,7 +126,12 @@ public class StatsWindow implements Initializable {
             morszczuk.getListAvg(type, AVG.Ao12).addListener((ListChangeListener<AVGwrapper>) change -> load());
         }
         for (CubeType type : types) {
-            morszczuk.getListAvg(type, AVG.Ao5).addListener((ListChangeListener<AVGwrapper>) change -> load());
+            morszczuk.getListAvg(type, AVG.Ao5).addListener(new ListChangeListener<AVGwrapper>() {
+                @Override
+                public void onChanged(Change<? extends AVGwrapper> change) {
+                    load();
+                }
+            });
         }
         load();
     }
